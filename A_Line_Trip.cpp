be09e80615507bde7f1511b170f4d671ecad3f41
unknown <aryan.shrivastva19@gmail.com>
2026-07-21 20:35:27 +0530

    setup

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+constexpr int INF = 1e9;
+constexpr ll LINF = 1e18;
+constexpr int MOD = 1e9 + 7;
+constexpr int MOD2 = 998244353;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+int solve() {
+    int n;
+    cin >> n;
+
+    // code
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << '\n';
+    }
+}
