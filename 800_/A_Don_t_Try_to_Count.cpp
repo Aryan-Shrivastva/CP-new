74c70c78e200a63250520e979739fcb69f7bb63e
unknown <aryan.shrivastva19@gmail.com>
2026-07-24 23:45:41 +0530

    2nd way to solve 1881A

--- 800_/A_Don_t_Try_to_Count.cpp
+++ 800_/A_Don_t_Try_to_Count.cpp
@@ -18,18 +18,19 @@ constexpr int MOD2 = 998244353;
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 
-bool check(string s, string x){
-    // If x is smaller than s, s cannot be a substring of x
-    if(x.size()<s.size()) return false;
+// bool check(string s, string x){
+//     // If x is smaller than s, s cannot be a substring of x
+//     if(x.size()<s.size()) return false;
 
-    // looping thrugh x to find if s is a substring
-    for(int i=0; i<x.size()-s.size()+1; i++){ // n - m + 1
+//     // looping thrugh x to find if s is a substring
+//     for(int i=0; i<x.size()-s.size()+1; i++){ // n - m + 1
 
-        if(x.substr(i, s.size())==s) return true;
-    }
+//         if(x.substr(i, s.size())==s) return true; //It means: Starting from start_index, 
+//                                                   //take length characters and return them as a new string.
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
 ll solve() {
     ll n, m;
@@ -39,23 +40,46 @@ ll solve() {
     string s;
     cin>>s;
 
-    string x0 = x;
-    string x1 = x0+x0;
-    string x2 = x1+x1;
-    string x3 = x2+x2;
-    string x4 = x3+x3;
-    string x5 = x4+x4;
+    // string x0 = x;
+    // string x1 = x0+x0;
+    // string x2 = x1+x1;
+    // string x3 = x2+x2;
+    // string x4 = x3+x3;
+    // string x5 = x4+x4;
 
-    ll ans = -1;
+    // ll ans = -1;
 
-    if (check(s, x0)) ans=0;
-    else if (check(s, x1)) ans=1;
-    else if (check(s, x2)) ans=2;
-    else if (check(s, x3)) ans=3;
-    else if (check(s, x4)) ans=4;
-    else if (check(s, x5)) ans=5;
-    // code
-    return ans;
+    // if (check(s, x0)) ans=0;
+    // else if (check(s, x1)) ans=1;
+    // else if (check(s, x2)) ans=2;
+    // else if (check(s, x3)) ans=3;
+    // else if (check(s, x4)) ans=4;
+    // else if (check(s, x5)) ans=5;
+    // // code
+    // return ans;
+
+
+
+    int ans = 0;
+
+    while (true) {
+        if (x.find(s) != string::npos)
+            return ans;
+
+        if (x.size() >= s.size())
+            break;
+
+        x += x;
+        ans++;
+    }
+
+    x += x;
+    ans++;
+
+    if (x.find(s) != string::npos)
+        return ans;
+
+    return -1;
     
 }
 
